628c6b954ed8225165a160db9b42b018e2675d98
lecon20122 <lecon20122@gmail.com>
2023-10-09 18:55:32 +0300

    adding CreateCategoryCommand, Custom Responses

---
+++ Ticketo.TicketManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketo.TicketManagement.Application.Features.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+        }
+    }    
+}
